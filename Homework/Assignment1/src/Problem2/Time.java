6d6891b9aae3b457d95c53e6401d85bb9f353835
Evan <evandoug90@gmail.com>
2019-05-11 14:36:58 -0700

    Add Time class with passing tests.

---
+++ Homework/Assignment1/src/Problem2/Time.java
@@ -0,0 +1,103 @@
+package Problem2;
+
+/**
+ * Class Time represents a time of day in 24 hour increments, from 00:00:00 to 23:59:59.
+ * Time does not distinguish days. Therefore a duration between two Times cannot be more
+ * than 23hrs 59mins, 59secs.
+ */
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    /**
+     * Basic constructor for Time
+     */
+    public Time() {
+        this(0,0,0);
+    }
+
+    /**
+     * Primary constructor for Time
+     * @param hour the hour
+     * @param minute the minute
+     * @param second the second
+     */
+    public Time(int hour, int minute, int second) {
+        this.hour = Time.validateHour(hour);
+        this.minute = Time.validateMinSec(minute);
+        this.second = Time.validateMinSec(second);
+    }
+
+    /**
+     * validateHour ensures that any entered hour is between 0 and 23.
+     * Given a number outside the range, uses mod 24 to put it inside the range.
+     * @param hour the given hour
+     * @return a number in the range (0, 23)
+     */
+    private static int validateHour(int hour) {
+        if (hour >= 0) {
+            return hour % 24;
+        } else {
+            int rem = hour % 24;
+            return 24 + rem;  // rem will be negative
+        }
+    }
+
+    /**
+     * validateMinSec ensures that any entered minute or second is between 0 and 59.
+     * Given a number outside the range, uses mod 60 to put it inside the range.
+     * @param number the given minute or second
+     * @return a number in the range (0, 59)
+     */
+    private static int validateMinSec(int number) {
+        if (number >= 0) {
+            return number % 60;
+        } else {
+            int rem = number % 60;
+            return 60 + rem;  // rem will be negative
+        }
+    }
+
+    /**
+     * @return The current hour
+     */
+    public int getHour() {
+        return hour;
+    }
+
+    /**
+     * @param hour the new hour
+     */
+    public void setHour(int hour) {
+        this.hour = validateHour(hour);
+    }
+
+    /**
+     * @return the current minute
+     */
+    public int getMin() {
+        return minute;
+    }
+
+    /**
+     * @param minute the new minute
+     */
+    public void setMin(int minute) {
+        this.minute = validateMinSec(minute);
+    }
+
+    /**
+     * @return the current second
+     */
+    public int getSec() {
+        return second;
+    }
+
+    /**
+     * @param second the new second
+     */
+    public void setSec(int second) {
+        this.second = validateMinSec(second);
+    }
+}
---
+++ Homework/Assignment1/src/Problem2/TimeTest.java
@@ -0,0 +1,89 @@
+package Problem2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimeTest {
+    private Time t1;
+    private Time t2;
+
+    @Before
+    public void setUp() {
+        t1 = new Time();
+        t2 = new Time(1, 30, 59);
+    }
+
+    @Test
+    public void TestBadSets() {
+        Time test = new Time(0, 0, -1);
+        // test should be 00:00:59
+        assertEquals(59, test.getSec());  // second
+        assertEquals(0, test.getMin());  // minute
+        assertEquals(0, test.getHour()); // hour
+
+        test = new Time(-1, -1, -1);
+        // test should be 23:59:59
+        assertEquals(59, test.getSec());  // second
+        assertEquals(59, test.getMin());  // minute
+        assertEquals(23, test.getHour()); // hour
+    }
+
+    @Test
+    public void getHour() {
+        assertEquals(0, t1.getHour());
+        assertEquals(1, t2.getHour());
+    }
+
+    @Test
+    public void setHour() {
+        t2.setHour(23);
+        assertEquals(23, t2.getHour());
+        t2.setHour(15);
+        assertEquals(15, t2.getHour());
+
+        t2.setHour(44);
+        assertEquals(20, t2.getHour());
+        t2.setHour(-30);
+        assertEquals(18, t2.getHour());
+    }
+
+    @Test
+    public void getMin() {
+        assertEquals(0, t1.getMin());
+        assertEquals(30, t2.getMin());
+    }
+
+    @Test
+    public void setMin() {
+        t2.setMin(59);
+        assertEquals(59, t2.getMin());
+        t2.setMin(0);
+        assertEquals(0, t2.getMin());
+
+        t2.setMin(77);
+        assertEquals(17, t2.getMin());
+        t2.setMin(-30);
+        assertEquals(30, t2.getMin());
+    }
+
+    @Test
+    public void getSec() {
+        assertEquals(0, t1.getSec());
+        assertEquals(59, t2.getSec());
+    }
+
+    @Test
+    public void setSec() {
+        t2.setSec(0);
+        assertEquals(0, t2.getSec());
+        t2.setSec(30);
+        assertEquals(30, t2.getSec());
+
+        t2.setSec(250);
+        assertEquals(10, t2.getSec());
+        t2.setSec(-999);
+        assertEquals(21, t2.getSec());
+    }
+}
