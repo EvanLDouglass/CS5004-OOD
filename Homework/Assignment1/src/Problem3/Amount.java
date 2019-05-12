9c9ec2b932bc0c736cf812094d7e07e61aab978a
Evan <evandoug90@gmail.com>
2019-05-11 18:08:20 -0700

    Mid point for Problem3. Some testing for Amount.

---
+++ Homework/Assignment1/src/Problem3/Amount.java
@@ -0,0 +1,118 @@
+package Problem3;
+
+/**
+ * Class Amount represents an amount in a bank account.
+ * The value of the account cannot be negative.
+ * @author evandouglass
+ */
+public class Amount {
+    private int dollars;
+    private int cents;
+
+    /**
+     * Basic constructor for class Amount.
+     */
+    public Amount() {
+        this(0, 0);
+    }
+
+    /**
+     * Primary constructor for class Amount.
+     * @param dollars the amount of dollars
+     * @param cents the amount of cents
+     */
+    public Amount(int dollars, int cents) {
+        // Validate dollars
+        if (dollars < 0) {
+            // negative values revert to zero
+            dollars = 0;
+        }
+        // Validate cents
+        if (cents < 0) {
+            cents = 0;
+        } else if (cents > 99) {
+            // convert anything over 99 into dollars
+            dollars += (cents / 100);
+            cents = cents % 100;
+        }
+
+        this.dollars = dollars;
+        this.cents = cents;
+    }
+
+    /* ===== Methods ===== */
+    // TODO: Finish Implementations
+    /**
+     * Adds the given amount and returns the value without side affects.
+     * @param other an amount to add
+     * @return the current account value + the other account value
+     */
+    public Amount add(Amount other) {
+        return new Amount();
+    }
+
+    /**
+     * Subtracts the given amount and returns the value without side affects.
+     * @param other an amount to subtract
+     * @return the current account value - the other account value
+     */
+    public Amount subtract(Amount other) {
+        return new Amount();
+    }
+
+    /* ===== Overrides ===== */
+
+    @Override
+    public String toString() {
+        return "Amount{" +
+                "$" + dollars +
+                "." + cents +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Amount amount = (Amount) o;
+        return dollars == amount.dollars &&
+                cents == amount.cents;
+    }
+
+    /* ===== Getters and Setters ===== */
+
+    /**
+     * Gets the dollars in the account.
+     * @return the amount of dollars in the account
+     */
+    public int getDollars() {
+        return dollars;
+    }
+
+    /**
+     * Sets the dollar amount in the account.
+     * @param dollars the new amount of dollars in the account
+     */
+    public void setDollars(int dollars) {
+        if (dollars < 0) {
+            dollars = 0;
+        }
+        this.dollars = dollars;
+    }
+
+    /**
+     * Gets the cents value in the account.
+     * @return the cents value in the account
+     */
+    public int getCents() {
+        return cents;
+    }
+
+    /**
+     * Sets the cents value in the account.
+     * @param cents the new cents value
+     */
+    public void setCents(int cents) {
+        this.cents = cents;
+    }
+}
---
+++ Homework/Assignment1/src/Problem3/AmountTest.java
@@ -0,0 +1,139 @@
+package Problem3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AmountTest {
+
+    @Test
+    public void setUpAndTest() {
+        Amount a = new Amount(-100, -100);
+        assertEquals(new Amount(0, 0), a);
+
+        a = new Amount(-100, 45);
+        assertEquals(new Amount(0, 45), a);
+
+        a = new Amount(-100, 500);
+        assertEquals(new Amount(5, 0), a);
+
+        a = new Amount(100, 500);
+        assertEquals(new Amount(105, 0), a);
+
+        a = new Amount(100, 550);
+        assertEquals(new Amount(105, 50), a);
+    }
+
+    @Test
+    public void add() {
+        Amount a1 = new Amount();  // Zeroed
+        assertEquals(new Amount(), a1.add(new Amount()));
+        Amount a2 = new Amount(50, 10);
+        assertEquals(a2, a1.add(a2));
+
+        // Add after set dollars
+        a1.setDollars(100);
+        assertEquals(new Amount(150, 10), a1.add(a2));
+
+        // Add after set cents
+        a2.setCents(55);
+        assertEquals(new Amount(150, 55), a2.add(a1));
+
+        // Swap parameters
+        assertEquals(new Amount(150, 10), a2.add(a1));
+        // Add to self
+        assertEquals(new Amount(200, 0), a1.add(a1));
+
+        Amount a3 = new Amount(123, 45).add(new Amount(200, 50));
+        assertEquals(new Amount(323, 50), a3);
+        assertEquals(new Amount(350, 0), a3.add(new Amount(26, 50)));
+    }
+
+    @Test
+    public void subtract() {
+        Amount a1 = new Amount();  // Zeroed
+        assertEquals(new Amount(), a1.subtract(new Amount()));
+        Amount a2 = new Amount(50, 10);
+        assertEquals(a2, a2.subtract(a1));
+
+        // Add after set dollars
+        a1.setDollars(100);
+        assertEquals(new Amount(50, 10), a1.subtract(a2));
+
+        // Add after set cents
+        a1.setCents(55);
+        assertEquals(new Amount(50, 45), a1.subtract(a2));
+        a2.setCents(95);
+        assertEquals(new Amount(49, 60), a1.subtract(a2));
+
+        // Subtract from self
+        assertEquals(new Amount(), a1.subtract(a1));
+
+        Amount a3 = new Amount(123, 45).subtract(new Amount(20, 50));
+        assertEquals(new Amount(102, 95), a3);
+        assertEquals(new Amount(180, 45), a3.subtract(new Amount(22, 50)));
+
+        // When subtracting bigger numbers
+        a1 = new Amount(500, 0);
+        assertEquals(new Amount(), a2.subtract(a1));
+        a2 = new Amount(0, 5500);
+        assertEquals(new Amount(), a1.subtract(a2));
+    }
+
+    @Test
+    public void toString1() {
+    }
+
+    @Test
+    public void equals1() {
+    }
+
+    @Test
+    public void setAndGetCents() {
+        Amount a = new Amount();
+        assertEquals(0, a.getCents());
+        a = new Amount(40, 23);
+        assertEquals(23, a.getCents());
+
+        a.setCents(67);
+        assertEquals(67, a.getCents());
+        a.setCents(0);
+        assertEquals(0, a.getCents());
+        a.setCents(99);
+        assertEquals(99, a.getCents());
+
+
+        // Test bad sets
+        a.setCents(100);
+        assertEquals(0, a.getCents());
+        assertEquals(1, a.getDollars());
+        a.setCents(250);
+        assertEquals(50, a.getCents());
+        assertEquals(2, a.getDollars());
+
+        a.setCents(-400);
+        assertEquals(0, a.getCents());
+        // set should be isolated, so does not affect dollars
+    }
+
+    @Test
+    public void setAndGetDollars() {
+        Amount a = new Amount();
+        assertEquals(0, a.getDollars());
+        a = new Amount(40, 23);
+        assertEquals(40, a.getDollars());
+
+        a.setDollars(67);
+        assertEquals(67, a.getDollars());
+        a.setDollars(0);
+        assertEquals(0, a.getDollars());
+        a.setDollars(99);
+        assertEquals(99, a.getDollars());
+        a.setDollars(700);
+        assertEquals(700, a.getDollars());
+
+        // Test bad sets
+        a.setDollars(-500);
+        assertEquals(0, a.getDollars());
+    }
+}
