ff48cefa46b1394acac4e332beb015ad01be7b6f
EvanLDouglass <EvanLDouglass@gmail.com>
2019-05-20 21:33:29 -0700

    Update perishable tests

--- Homework/Assignment2/tests/problem3/PerishableFoodItemTest.java
+++ Homework/Assignment2/tests/problem3/PerishableFoodItemTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class PerishableFoodItemTest {
 
   @Test
-  public void testGetters() {
+  public void testConstructorAndGetters() {
     PerishableFoodItem item = new PerishableFoodItem("Perishable",
         34.5,
         90,
@@ -15,6 +15,26 @@ public class PerishableFoodItemTest {
         40);
     assertEquals(10, (int) item.getOrderDate());
     assertEquals(40, (int) item.getExpirationDate());
+
+    // Zero order date
+    item = new PerishableFoodItem("Perishable",
+        34.5,
+        90,
+        0,
+        1);
+    assertEquals(0, (int) item.getOrderDate());
+    assertEquals(1, (int) item.getExpirationDate());
+
+    // Negative order date, zero exp. date
+    // Negatives allowed in the event that something was ordered prior to
+    // deploying this system.
+    item = new PerishableFoodItem("Perishable",
+        34.5,
+        90,
+        -1,
+        0);
+    assertEquals(-1, (int) item.getOrderDate());
+    assertEquals(0, (int) item.getExpirationDate());
   }
 
   @Test(expected = IllegalArgumentException.class)
