8c64d925eebbd2b07e8593d66c6a3d1b4689181d
EvanLDouglass <EvanLDouglass@gmail.com>
2019-06-02 15:35:35 -0700

    Add GardeningService class

---
+++ Homework/assignment3/src/main/java/edu/neu/khoury/cs5004/problem2/GardeningService.java
@@ -0,0 +1,36 @@
+package edu.neu.khoury.cs5004.problem2;
+
+public class GardeningService extends AbstractExteriorService {
+
+  /**
+   * Constructor for an exterior gardening service.
+   *
+   * @param invoiceID the invoice ID for this service
+   * @param serviceAddress the address the address at which the service is performed
+   * @param propertySize the size of the property at which the service is performed
+   * @param isMonthlyService {@code true} if this service is performed monthly, else {@code false}
+   * @param numServicesAtAddr the number of times this service has been done at this address
+   * @param month the current month as an integer between 1 and 12, inclusive
+   */
+  public GardeningService(String invoiceID, String serviceAddress,
+      PropertySize propertySize, boolean isMonthlyService, Integer numServicesAtAddr,
+      Integer month) throws IllegalArgumentException {
+    super(invoiceID, serviceAddress, propertySize, isMonthlyService, numServicesAtAddr, month);
+  }
+
+  /* ===== Methods ===== */
+
+  @Override
+  public Double calculatePrice() {
+    Double wasteRemovalFee = 20.0;
+    return super.calculatePrice() + wasteRemovalFee;
+  }
+
+  /* ===== Object Overrides ===== */
+  // No additional fields so equals and hashcode are the same as super class
+
+  @Override
+  public String toString() {
+    return "GardeningService: " + super.toString();
+  }
+}
---
+++ Homework/assignment3/src/test/java/edu/neu/khoury/cs5004/problem2/GardeningServiceTest.java
@@ -0,0 +1,56 @@
+package edu.neu.khoury.cs5004.problem2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GardeningServiceTest {
+
+  private GardeningService service;
+
+  @Before
+  public void setUp() throws Exception {
+    service = new GardeningService(
+        "0001",
+        "1st Ave",
+        PropertySize.SMALL,
+        false,
+        1,
+        12
+    );
+  }
+
+  @Test
+  public void calculatePrice() {
+    assertEquals(180.0, service.calculatePrice(), 0.0);
+
+    service = new GardeningService(
+        "0001",
+        "1st Ave",
+        PropertySize.MEDIUM,
+        false,
+        1,
+        12
+    );
+    assertEquals(180.0, service.calculatePrice(), 0.0);
+
+    service = new GardeningService(
+        "0001",
+        "1st Ave",
+        PropertySize.LARGE,
+        false,
+        1,
+        12
+    );
+    assertEquals(340.0, service.calculatePrice(), 0.0);
+  }
+
+  @Test
+  public void toString1() {
+    String result =
+        "GardeningService: invoice=0001 address='1st Ave' size=Small "
+        + "monthly=false serviced=1 month=12";
+    assertEquals(result, service.toString());
+  }
+}
