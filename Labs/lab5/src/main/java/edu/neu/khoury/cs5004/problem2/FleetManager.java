448aa9816d296b27f75d6c10a8f6361b7e6b14ca
EvanLDouglass <EvanLDouglass@gmail.com>
2019-06-13 21:21:36 -0700

    Add overloaded methods in FleetManager for lab6

--- Labs/lab5/src/main/java/edu/neu/khoury/cs5004/problem2/FleetManager.java
+++ Labs/lab5/src/main/java/edu/neu/khoury/cs5004/problem2/FleetManager.java
@@ -19,7 +19,55 @@ public class FleetManager {
         vehicle,
         vehicle.getAvgSpeed(),
         distance,
-        (int) (distance/vehicle.getAvgSpeed())
+        (int) (distance / vehicle.getAvgSpeed())
+    );
+  }
+
+  /**
+   * Creates a trip report given a distance and boat.
+   *
+   * @param distance the distance the vehicle has traveled
+   * @param boat the vehicle
+   * @return a trip report
+   */
+  public TripReport drive(float distance, Boat boat) {
+    return new TripReport(
+        boat,
+        boat.getAvgSpeed(),
+        distance,
+        (int) (distance / boat.getAvgSpeed())
+    );
+  }
+
+  /**
+   * Creates a trip report given a distance and bus.
+   *
+   * @param distance the distance the vehicle has traveled
+   * @param bus the vehicle
+   * @return a trip report
+   */
+  public TripReport drive(float distance, Bus bus) {
+    return new TripReport(
+        bus,
+        bus.getAvgSpeed(),
+        distance,
+        (int) (distance / bus.getAvgSpeed())
+    );
+  }
+
+  /**
+   * Creates a trip report given a distance and train.
+   *
+   * @param distance the distance the vehicle has traveled
+   * @param train the vehicle
+   * @return a trip report
+   */
+  public TripReport drive(float distance, Train train) {
+    return new TripReport(
+        train,
+        train.getAvgSpeed(),
+        distance,
+        (int) (distance / train.getAvgSpeed())
     );
   }
 }
--- Labs/lab5/src/test/java/edu/neu/khoury/cs5004/problem2/FleetManagerTest.java
+++ Labs/lab5/src/test/java/edu/neu/khoury/cs5004/problem2/FleetManagerTest.java
@@ -1,6 +1,6 @@
 package edu.neu.khoury.cs5004.problem2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,11 +15,44 @@ public class FleetManagerTest {
   }
 
   @Test
-  public void drive() {
+  public void driveBoat() {
     Boat boat = new Boat("12345", (float) 25.0, (float) 50.0);
     TripReport report = mngr.drive((float) 100.0, boat);
 
     assertEquals(boat, report.getVehicle());
+    assertEquals((float) 25.0, report.getSpeed(), 0.0);
+    assertEquals((float) 100.0, report.getDistance(), 0.0);
+    assertEquals(4, (int) report.getDuration());
+  }
+
+  @Test
+  public void driveBus() {
+    Bus bus = new Bus("12345", (float) 25.0, (float) 50.0);
+    TripReport report = mngr.drive((float) 100.0, bus);
+
+    assertEquals(bus, report.getVehicle());
+    assertEquals((float) 25.0, report.getSpeed(), 0.0);
+    assertEquals((float) 100.0, report.getDistance(), 0.0);
+    assertEquals(4, (int) report.getDuration());
+  }
+
+  @Test
+  public void driveTrain() {
+    Train train = new Train("12345", (float) 25.0, (float) 50.0);
+    TripReport report = mngr.drive((float) 100.0, train);
+
+    assertEquals(train, report.getVehicle());
+    assertEquals((float) 25.0, report.getSpeed(), 0.0);
+    assertEquals((float) 100.0, report.getDistance(), 0.0);
+    assertEquals(4, (int) report.getDuration());
+  }
+
+  @Test
+  public void driveVehicle() {
+    Vehicle vehicle = new Boat("12345", (float) 25.0, (float) 50.0);
+    TripReport report = mngr.drive((float) 100.0, vehicle);
+
+    assertEquals(vehicle, report.getVehicle());
     assertEquals((float) 25.0, report.getSpeed(), 0.0);
     assertEquals((float) 100.0, report.getDistance(), 0.0);
     assertEquals(4, (int) report.getDuration());
